171c8a3c3a0abaf55b70c8ed4c7425df8ebadf21
PRM Greefhorst <p.r.m.greefhorst@student.vu.nl>
2019-06-06 23:27:28 +0200

    no message

--- public/js/fuzzy.js
+++ public/js/fuzzy.js
@@ -1,6 +1,4 @@
-function searchNicknames(searchBar){
-	var value = searchBar;
-	var books = [{
+var artists = [{
 	  'artist': "Vincent van Gogh",
 	  'nicknames': ['Vincie van cogh']
 	}, {
@@ -8,29 +6,34 @@ function searchNicknames(searchBar){
 	  'nicknames': ['Brandrem van Rijn']
 	}]
 
-	var options = {
-	  keys: ['artist', 'nicknames']
-	};
+var options = {
+  keys: ['artist', 'nicknames']
+};
 
-	var fuse = new Fuse(books, options);
+var fuse = new Fuse(artists, options);
 
+function searchNicknames(searchBar){
 	var result = document.getElementById("result");
-	resultArtist = (fuse.search(searchBar)[0]);
+	window.searchBar = searchBar;
+	window.resultArtist = (fuse.search(searchBar)[0]);
+
 	if(resultArtist['artist']=='Vincent van Gogh'){
 		document.getElementById('nameresult').style.background = "#f3f3f3 url('graphics/vincent.jpg') no-repeat";
 		document.getElementById('nameresult').style.backgroundSize = 'cover';
-		var id = 'vincent_van_gogh';
+		window.artistID = 'vincent_van_gogh';
 	}
 	if(resultArtist['artist']=='Rembrandt van Rijn'){
 		document.getElementById('nameresult').style.background = "#f3f3f3 url('graphics/rembrandt.jpg') no-repeat";
 		document.getElementById('nameresult').style.backgroundSize = 'cover';
-		var id = 'rembrandt_van_rijn';
+		window.artistID = 'rembrandt_van_rijn';
 	}	
-	result.innerHTML = '<h2>So your looking for: ' + resultArtist['artist'] + '?</h2>';
+	result.innerHTML = '<div><img src="./graphics/vincentpic.jpg" height="200px"></div><h2>So your looking for: ' + resultArtist['artist'] + '?</h2>';
+}
 
+function addToDatabase(){
 	var db = firebase.firestore()
-	db.collection("artists").doc(id).collection("fuzzynames").add({
-		fuzzy: value
+	db.collection("artists").doc(window.artistID).collection("fuzzynames").add({
+		fuzzy: window.searchBar
 	})
 	.then(function(docRef) {
 	    console.log("Document written with ID: ", docRef.id);
@@ -39,16 +42,3 @@ function searchNicknames(searchBar){
 	    console.error("Error adding document: ", error);
 	});
 }
-//   // Get a reference to the database service
-// function firebaseE(value, id){
-// 	var db = firebase.firestore()
-// 	db.collection("artists").doc(id).collection("fuzzynames").add({
-// 		fuzzy: value
-// 	})
-// 	.then(function(docRef) {
-// 	    console.log("Document written with ID: ", docRef.id);
-// 	})
-// 	.catch(function(error) {
-// 	    console.error("Error adding document: ", error);
-// 	});
-// }
